e64ccd001b087974696215a6a9846ab0b1a1ec16
tuutoo <tuutoo@iMac.com>
2023-01-10 23:12:27 +0800

    done 11-1

--- day11/1.ts
+++ day11/1.ts
@@ -8,8 +8,17 @@ const fileContent = fs.readFileSync(inputFile, 'utf8')
 // 将文件内容按行分割成数组
 const lines = fileContent.split('\n')
 
-let sharedBucket = [[79, 98], [54, 65, 75, 74], [79, 60, 97], [74]]
-let itemInspected = [0 ,0 ,0 ,0]
+let sharedBucket = [
+  [56, 52, 58, 96, 70, 75, 72]
+  ,[75, 58, 86, 80, 55, 81]
+  ,[73, 68, 73, 90]
+  ,[72, 89, 55, 51, 59]
+  ,[76, 76, 91]
+  ,[88]
+  ,[64, 63, 56, 50, 77, 55, 55, 86]
+  ,[79, 58]
+]
+let itemInspected = [0 ,0 ,0 ,0, 0 ,0 ,0 ,0]
 
 abstract class Monkey {
   id: number = -1
@@ -23,7 +32,15 @@ abstract class Monkey {
 
   abstract operation(): void
   abstract test(): void
-  abstract throw(): void
+
+  throw() {
+    this.worriedItems.map((item, index) => {
+      sharedBucket[this.throwTo[index]].push(item)
+      sharedBucket[this.id].pop()
+      itemInspected[this.id] += 1
+    })
+  }
+
   clean() {
     this.startingItems = []
     this.newItems = []
@@ -49,21 +66,13 @@ class Monkey0 extends Monkey {
 
   operation() {
     this.startingItems = sharedBucket[this.id]
-    this.newItems = this.startingItems.map(x => x * 19)
+    this.newItems = this.startingItems.map(x => x * 17)
     this.worriedItems = this.newItems.map(x => Math.floor(x / 3))
   }
 
   test() {
-    this.testResults = this.worriedItems.map(x => (x % 23 === 0))
+    this.testResults = this.worriedItems.map(x => (x % 11 === 0))
     this.throwTo = this.testResults.map(x => x === true ? 2 : 3)
-  }
-
-  throw() {
-    this.worriedItems.map((item, index) => {
-      sharedBucket[this.throwTo[index]].push(item)
-      sharedBucket[this.id].pop()
-      itemInspected[this.id] += 1
-    })
   }
 }
 
@@ -75,21 +84,13 @@ class Monkey1 extends Monkey {
 
   operation() {
     this.startingItems = sharedBucket[this.id]
-    this.newItems = this.startingItems.map(x => x + 6)
+    this.newItems = this.startingItems.map(x => x + 7)
     this.worriedItems = this.newItems.map(x => Math.floor(x / 3))
   }
 
   test() {
-    this.testResults = this.worriedItems.map(x => (x % 19 === 0))
-    this.throwTo = this.testResults.map(x => x === true ? 2 : 0)
-  }
-
-  throw() {
-    this.worriedItems.map((item, index) => {
-      sharedBucket[this.throwTo[index]].push(item)
-      sharedBucket[this.id].pop()
-      itemInspected[this.id] += 1
-    })
+    this.testResults = this.worriedItems.map(x => (x % 3 === 0))
+    this.throwTo = this.testResults.map(x => x === true ? 6 : 5)
   }
 }
 
@@ -106,16 +107,8 @@ class Monkey2 extends Monkey {
   }
 
   test() {
-    this.testResults = this.worriedItems.map(x => (x % 13 === 0))
-    this.throwTo = this.testResults.map(x => x === true ? 1 : 3)
-  }
-
-  throw() {
-    this.worriedItems.map((item, index) => {
-      sharedBucket[this.throwTo[index]].push(item)
-      sharedBucket[this.id].pop()
-      itemInspected[this.id] += 1
-    })
+    this.testResults = this.worriedItems.map(x => (x % 5 === 0))
+    this.throwTo = this.testResults.map(x => x === true ? 1 : 7)
   }
 }
 
@@ -127,21 +120,85 @@ class Monkey3 extends Monkey {
 
   operation() {
     this.startingItems = sharedBucket[this.id]
-    this.newItems = this.startingItems.map(x => x + 3)
+    this.newItems = this.startingItems.map(x => x + 1)
+    this.worriedItems = this.newItems.map(x => Math.floor(x / 3))
+  }
+
+  test() {
+    this.testResults = this.worriedItems.map(x => (x % 7 === 0))
+    this.throwTo = this.testResults.map(x => x === true ? 2 : 7)
+  }
+}
+
+class Monkey4 extends Monkey {
+  constructor() {
+    super()
+    this.id = 4
+  }
+
+  operation() {
+    this.startingItems = sharedBucket[this.id]
+    this.newItems = this.startingItems.map(x => x * 3)
+    this.worriedItems = this.newItems.map(x => Math.floor(x / 3))
+  }
+
+  test() {
+    this.testResults = this.worriedItems.map(x => (x % 19 === 0))
+    this.throwTo = this.testResults.map(x => x === true ? 0 : 3)
+  }
+}
+
+class Monkey5 extends Monkey {
+  constructor() {
+    super()
+    this.id = 5
+  }
+
+  operation() {
+    this.startingItems = sharedBucket[this.id]
+    this.newItems = this.startingItems.map(x => x + 4)
+    this.worriedItems = this.newItems.map(x => Math.floor(x / 3))
+  }
+
+  test() {
+    this.testResults = this.worriedItems.map(x => (x % 2 === 0))
+    this.throwTo = this.testResults.map(x => x === true ? 6 : 4)
+  }
+}
+
+class Monkey6 extends Monkey {
+  constructor() {
+    super()
+    this.id = 6
+  }
+
+  operation() {
+    this.startingItems = sharedBucket[this.id]
+    this.newItems = this.startingItems.map(x => x + 8)
+    this.worriedItems = this.newItems.map(x => Math.floor(x / 3))
+  }
+
+  test() {
+    this.testResults = this.worriedItems.map(x => (x % 13 === 0))
+    this.throwTo = this.testResults.map(x => x === true ? 4 : 0)
+  }
+}
+
+class Monkey7 extends Monkey {
+  constructor() {
+    super()
+    this.id = 7
+  }
+
+  operation() {
+    this.startingItems = sharedBucket[this.id]
+    this.newItems = this.startingItems.map(x => x + 6)
     this.worriedItems = this.newItems.map(x => Math.floor(x / 3))
   }
 
   test() {
     this.testResults = this.worriedItems.map(x => (x % 17 === 0))
-    this.throwTo = this.testResults.map(x => x === true ? 0 : 1)
-  }
-
-  throw() {
-    this.worriedItems.map((item, index) => {
-      sharedBucket[this.throwTo[index]].push(item)
-      sharedBucket[this.id].pop()
-      itemInspected[this.id] += 1
-    })
+    this.throwTo = this.testResults.map(x => x === true ? 1 : 5)
   }
 }
 
@@ -149,14 +206,22 @@ let monkey0 = new Monkey0();
 let monkey1 = new Monkey1();
 let monkey2 = new Monkey2();
 let monkey3 = new Monkey3();
+let monkey4 = new Monkey4();
+let monkey5 = new Monkey5();
+let monkey6 = new Monkey6();
+let monkey7 = new Monkey7();
 
 for (let i = 0; i < 20; i++) {
   monkey0.play()
   monkey1.play()
   monkey2.play()
   monkey3.play()
+  monkey4.play()
+  monkey5.play()
+  monkey6.play()
+  monkey7.play()
 }
 
 let sum = itemInspected.sort((a, b) => b-a).slice(0,2).reduce((a,b)=> a*b)
 
-console.log(monkey0, monkey1, monkey2, monkey3, sharedBucket, itemInspected, sum);
+console.log(sharedBucket, itemInspected, sum);
